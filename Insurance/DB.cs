867d2931b744c4ead716a94a60aa5062b4bf2e88
DulguunEnkhbat <b21fa1732@ufe.edu.mn>
2025-05-29 11:49:37 +0800

    Admin

---
+++ Insurance/DB.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data;
+using System.Data.SqlClient;
+
+namespace Insurance
+{
+     class DB
+    {
+        public SqlConnection con = new SqlConnection();
+        public SqlCommand cmd = new SqlCommand();
+        public DataSet ds = new DataSet();
+        public SqlDataAdapter ada = new SqlDataAdapter();
+        public SqlDataReader dr;
+
+        public DB()
+        {
+            if (con.State != ConnectionState.Open)
+            {
+                con.Close();
+                con.Dispose();
+
+            }
+            con.ConnectionString = @"Data Source = 172.16.35.45; Initial Catalog = Insurance_New; User ID = sa; Password = 123";
+            con.Open();
+            cmd.Connection = con;
+        }
+    }
+}
--- Insurance/Form1.Designer.cs
+++ Insurance/Form1.Designer.cs
@@ -34,10 +34,10 @@
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(180, 133);
-            this.button1.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.button1.Location = new System.Drawing.Point(120, 86);
+            this.button1.Margin = new System.Windows.Forms.Padding(1, 1, 1, 1);
             this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(159, 63);
+            this.button1.Size = new System.Drawing.Size(106, 41);
             this.button1.TabIndex = 0;
             this.button1.Text = "Sudalgaa";
             this.button1.UseVisualStyleBackColor = true;
@@ -45,21 +45,23 @@
             // 
             // button2
             // 
-            this.button2.Location = new System.Drawing.Point(455, 133);
-            this.button2.Margin = new System.Windows.Forms.Padding(2);
+            this.button2.Location = new System.Drawing.Point(303, 86);
+            this.button2.Margin = new System.Windows.Forms.Padding(1, 1, 1, 1);
             this.button2.Name = "button2";
-            this.button2.Size = new System.Drawing.Size(159, 63);
+            this.button2.Size = new System.Drawing.Size(106, 41);
             this.button2.TabIndex = 1;
             this.button2.Text = "Login";
             this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
             // 
             // Form1
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(927, 590);
+            this.ClientSize = new System.Drawing.Size(618, 384);
             this.Controls.Add(this.button2);
             this.Controls.Add(this.button1);
+            this.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.Name = "Form1";
             this.Text = "Form1";
             this.ResumeLayout(false);
