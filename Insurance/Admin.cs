6c87d3924fedf55a40c053d5e7210fff6fdcebc0
DulguunEnkhbat <b21fa1732@ufe.edu.mn>
2025-05-29 13:30:25 +0800

    Commit

--- Insurance/Admin.cs
+++ Insurance/Admin.cs
@@ -19,7 +19,61 @@ namespace Insurance
 
         private void treeView1_AfterSelect(object sender, TreeViewEventArgs e)
         {
+            splitContainer1.Panel2.Controls.Clear();
 
+            UserControl selectedReport = null;
+
+            switch (e.Node.Index)
+            {
+                case 1:
+                    selectedReport = new UserControlAccident();
+                    break;
+                case 2:
+                    selectedReport = new UserControlAccident();
+                    break;
+                case 3:
+                    selectedReport = new UserControlAccident();
+                    break;
+                case 4:
+                    selectedReport = new UserControlAccident();
+                    break;
+                case 6:
+                    selectedReport = new UserControlAccident();
+                    break;
+                case 7:
+                    selectedReport = new UserControlAccident();
+                    break;
+                case 9:
+                    selectedReport = new UserControlAccident();
+                    break;
+                case 10:
+                    selectedReport = new UserControlAccident();
+                    break;
+                case 11:
+                    selectedReport = new UserControlAccident();
+                    break;
+                case 12:
+                    selectedReport = new UserControlAccident();
+                    break;
+                case 13:
+                    selectedReport = new UserControlAccident();
+                    break;
+                case 14:
+                    selectedReport = new UserControlAccident();
+                    break;
+                case 15:
+                    selectedReport = new UserControlAccident();
+                    break;
+                default:
+                    break;
+            }
+
+            if (selectedReport != null)
+            {
+                selectedReport.Dock = DockStyle.Fill;
+                splitContainer1.Panel2.Controls.Add(selectedReport);
+            }
         }
     }
-}
+
+    }
---
+++ Insurance/UserControlDocuments.Designer.cs
@@ -0,0 +1,45 @@
+﻿namespace Insurance
+{
+    partial class UserControlDocuments
+    {
+        /// <summary> 
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary> 
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Component Designer generated code
+
+        /// <summary> 
+        /// Required method for Designer support - do not modify 
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.SuspendLayout();
+            // 
+            // UserControlDocuments
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.Name = "UserControlDocuments";
+            this.Size = new System.Drawing.Size(674, 433);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+    }
+}
